0de613c20d86e8dd8aa9a29261f0d31d667e22db
idben <idben.chen@gmail.com>
2024-04-25 15:06:08 +0800

    簡單的 API 輸出與測試

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import multer from 'multer';
+
+const upload = multer();
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  if (req.method === "POST" && req.headers["content-type"].startsWith("multipart/form-data")) {
+    upload.none()(req, res, (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'Multer error: ' + err.message });
+      }
+      const {account, password} = req.body;
+      res.status(200).json({ account, password });
+    });
+  }else{
+    res.status(200).json({ name: "John Doe" });
+  }
 }
